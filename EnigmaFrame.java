05f68a43a83faca7394bf658532247dfd2a66e65
Masih Faryadi <masih22@masihs-mbp.mynetworksettings.com>
2024-12-01 18:25:50 -0500

    Initializing all required variables for the GUI, as well as setting BorderLayout

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -7,7 +7,7 @@ public class EnigmaFrame extends JFrame {
     //JComboBox for selecting rotor numbers 
     private JComboBox<Integer> inRotor, outRotor, midRotor;
     //JTextField for selecting start of Rotors
-    private JTextField startPosition;
+    private JTextField initialPosition;
     //JTextArea for providing input to and output from Enigma
     private JTextArea input, output;
     //JButton for selecting encrypt or decrypt
@@ -16,6 +16,27 @@ public class EnigmaFrame extends JFrame {
     private JLabel inRotorLabel, outRotorLabel, midRotorLabel, positionLabel, inputLabel, outputLabel;
 
     public EnigmaFrame() {
+        //set border layout 
+        this.setLayout(new BorderLayout());
+        //JPanel for our controls 
+        JPanel settings = new JPanel(new FlowLayout());
+        //Initializing rotors and their choices 1-5
+        Integer[] choices = {1,2,3,4,5};
+        inRotor = new JComboBox<>(choices);
+        midRotor = new JComboBox<>(choices);
+        outRotor = new JComboBox<>(choices);
+        //initialize JTextfield
+        initialPosition = new JTextField("EST", 3);
+        //initialize JButtons
+        encrypt = new JButton("Encrypt");
+        decrypt = new JButton("Decrypt");
+        //initialize JLabels
+        inRotorLabel = new JLabel("Inner");
+        midRotorLabel = new JLabel("Middle");
+        outRotorLabel = new JLabel("Outer");
+        positionLabel = new JLabel("Initial Positions");
+        inputLabel = new JLabel("Input");
+        outputLabel = new JLabel("Output");
         
     }
 }
